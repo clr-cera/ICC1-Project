f1ec16a11dbf81ae9df0f45834368cbfa79541bd
clr-cera <93736542+clr-cera@users.noreply.github.com>
2023-06-28 17:25:09 -0300

    Struct Implemented!

--- main.c
+++ main.c
@@ -2,40 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct _produto{ //Esse é o struct para um tipo de produto
+  int quantidade;
+  float preco;
+  char* nome;
+  int codigo;
+}produto;
+
+
 /*
  Esta função insere os produtos nos vetores, 
  ela copia os valores passados para o item no indice nos vetores passados
 */
-void insere_produto(char* nome, int quantidade, float preco, char** nomes, int* quantidades, float* precos, int indice){ 
-  nomes[indice] = (char *) malloc(100 * sizeof(char)); //Cria o vetor do nome dentro de nomes
+void insere_produto(char* nome, int quantidade, float preco, produto* produtoPonteiro){ 
+  produtoPonteiro->nome = (char *) malloc(100 * sizeof(char)); //Cria o vetor do nome dentro do produto
   for (int i = 0; i < 100; i++) 
-    nomes[indice][i] = nome[i]; //Copia o nome, do vetor passado (nome) para o vetor criado (nomes)
+    produtoPonteiro->nome[i] = nome[i]; //Copia o nome, do vetor passado (nome) para o vetor criado dentro do produto
 
 
-  quantidades[indice] = quantidade; //Recebe a quantidade
-  precos[indice] = preco; //Recebe o preco
+  produtoPonteiro->quantidade = quantidade; //Recebe a quantidade
+  produtoPonteiro->preco = preco; //Recebe o preco
 }
 
 // Esta função aumenta a quantidade do produto do codigo informado
-void aumenta_estoque(int codigo, int quantidade, int* quantidades){
-  quantidades[codigo] += quantidade;  
+void aumenta_estoque(int quantidade, produto* produtoPonteiro){
+  produtoPonteiro->quantidade += quantidade;  
 }
 
 // Esta função altera o preço do produto do codigo informado
-void modifica_preco(int codigo, float preco, float* precos){
-  precos[codigo] = preco;
+void modifica_preco(float preco, produto* produtoPonteiro){
+  produtoPonteiro->preco = preco;
 }
 
 // Esta funcao vende produtos
-float venda(int codigo, float saldo, float* precos, char** nomes, int* quantidades,float* total){ //Ela recebe o codigo do produto que foi vendido, o vetor de precos e o vetor de nomes
+float venda(int codigo, float saldo, produto* produtoPonteiro, float* total){ //Ela recebe o codigo do produto que foi vendido, o vetor de precos e o vetor de nomes
 
   if (codigo != -1){ //Se o codigo nao for -1 ele vai vender o produto (aumentar o saldo, aumentar o total da venda, e diminuir 1 da quantidade)
-  saldo += precos[codigo];
-  *total += precos[codigo];
-  quantidades[codigo] -= 1;
+  saldo += produtoPonteiro->preco;
+  *total += produtoPonteiro->preco;
+  produtoPonteiro->quantidade -= 1;
   
   
-  printf("%s %.2f\n", nomes[codigo], precos[codigo]); //imprime a venda
+  printf("%s %.2f\n", produtoPonteiro->nome, produtoPonteiro->preco); //imprime a venda
   }
   else printf("Total: %.2f\n", *total); //Se o codigo for -1 ele imprime o saldo resultante
 
@@ -43,16 +51,16 @@ float venda(int codigo, float saldo, float* precos, char** nomes, int* quantidad
 }
 
 //Essa função apresenta o estoque de produtos com suas
-void consulta_estoque(char **nomes, int *quantidades, int indice){
-  for(int codigo = 0; codigo<indice; codigo++){ 
+void consulta_estoque(produto* produtoLista, int indice){
+  for(int i = 0; i<indice; i++){ 
     //Apresenta na tela os valores conforme cada produto (indexado pelo código)
-    printf("%d %s %d\n", codigo, nomes[codigo], quantidades[codigo]); 
+    printf("%d %s %d\n", i, produtoLista[i].nome, produtoLista[i].quantidade); 
   }
 }
 
 //Essa função apresenta o saldo total
-void consultar_saldo(int saldo){
-  printf("Saldo: %d", saldo);
+void consultar_saldo(float saldo){
+  printf("Saldo: %.2f\n", saldo);
 }
 
 int main(void){
@@ -60,16 +68,13 @@ int main(void){
   int estoque;
   int indice = 0; //Este é o índice para a inserção de novos produtos (o código dele)
 
-  char** nomes; //Vetores para dicionario
-  int* quantidades;
-  float* precos;
+  produto* produtoVetor;
 
   scanf("%d", &estoque); //Recebe numero de diferentes tipos de produtos
 
-  //Alocação dos vetores:
-  nomes = (char **) malloc(estoque * sizeof(char*)); 
-  quantidades = (int *) malloc(estoque * sizeof(int));
-  precos = (float *) malloc(estoque * sizeof(float));
+  //Alocação do vetor:
+  produtoVetor = (produto *) malloc(estoque*sizeof(produto));
+  
 
 
   while(1){
@@ -81,7 +86,7 @@ int main(void){
       char nome[100]; int quantidade; float preco;
       scanf("%s %d %f", nome, &quantidade, &preco); //Recebe os valores
       
-      insere_produto(nome, quantidade, preco, nomes, quantidades, precos, indice); //Insere os valores nos vetores 
+      insere_produto(nome, quantidade, preco, &produtoVetor[indice]); //Insere os valores no vetor
       indice++; //Aumenta o índice para a proxima inserção
 
     }
@@ -91,7 +96,7 @@ int main(void){
       int codigo; int quantidade;
       scanf("%d %d", &codigo, &quantidade); //Recebe os valores
 
-      aumenta_estoque(codigo, quantidade, quantidades); //Coloca no vetor quantidades
+      aumenta_estoque(quantidade, &produtoVetor[codigo]); //Coloca no vetor quantidades
     }
     
     if(strcmp(comando, "MP")==0) // COMANDO DE MODIFCAR PREÇO
@@ -99,7 +104,7 @@ int main(void){
       int codigo; float preco;
       scanf("%d %f", &codigo, &preco); //Recebe os valores
 
-      modifica_preco(codigo, preco, precos); //Coloca no vetor precos
+      modifica_preco(preco, &produtoVetor[codigo]); //Coloca no vetor precos
     }
     
     if(strcmp(comando, "VE")==0) // COMANDO DE VENDER PRODUTOS
@@ -110,7 +115,7 @@ int main(void){
       while(codigo != -1){ //Quando receber o codigo -1, vai executar pela ultima vez
         scanf("%d", &codigo);
 
-        saldo = venda(codigo, saldo, precos, nomes, quantidades, &total); //A função vai retornar sempre o saldo apos vender uma unidade do produto, aumentar o total da venda alterar a quantidade no estoque e imprimir a saída necessária, caso o codigo seja -1, irá apenas imprimir o saldo
+        saldo = venda(codigo, saldo, &produtoVetor[codigo], &total); //A função vai retornar sempre o saldo apos vender uma unidade do produto, aumentar o total da venda alterar a quantidade no estoque e imprimir a saída necessária, caso o codigo seja -1, irá apenas imprimir o saldo
       }
 
       printf("--------------------------------------------------\n"); 
@@ -118,13 +123,13 @@ int main(void){
     
     if(strcmp(comando, "CE")==0) // COMANDO DE CONSULTAR ESTOQUE
     {
-      consulta_estoque(nomes, quantidades, indice); //Recebe os parametros necessários para a função void apresentar os valores
+      consulta_estoque(produtoVetor, indice); //Recebe os parametros necessários para a função void apresentar os valores
       printf("--------------------------------------------------\n"); 
     }
     
     if(strcmp(comando, "CS")==0) // COMANDO DE CONSULTAR SALDO
     {
-      consulta_saldo(saldo); //O saldo é passado por paramêtro apenas para apresenta-lo na função
+      consultar_saldo(saldo); //O saldo é passado por parâmetro apenas para apresenta-lo na função
       printf("--------------------------------------------------\n"); 
     }
 
@@ -134,11 +139,9 @@ int main(void){
     }
   }
   //Liberação da memória alocada dinamicamente na Heap
-  free(precos); 
-  free(quantidades);
   for(int i = 0; i < indice; i++)
-    free(nomes[i]);
-  free(nomes);
+    free(produtoVetor[i].nome);
+  free(produtoVetor);
   
   return 0;
 }
